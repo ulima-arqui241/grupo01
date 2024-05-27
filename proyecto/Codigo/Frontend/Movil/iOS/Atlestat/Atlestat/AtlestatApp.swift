8e2b8a75903cf90ef349de68bd3fc6c32793a6ec
FrancoM564 <franco1.alfra@gmail.com>
2024-05-27 09:02:39 -0500

    initial project ios

---
+++ proyecto/Codigo/Frontend/Movil/iOS/Atlestat/Atlestat/AtlestatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlestatApp.swift
+//  Atlestat
+//
+//  Created by Franco Marquez(Personal iCloud) on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AtlestatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ proyecto/Codigo/Frontend/Movil/iOS/Atlestat/Atlestat/Scenes/Login/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Atlestat
+//
+//  Created by Franco Marquez(Personal iCloud) on 27/05/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
